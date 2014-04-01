c63f03edc2d460d2d3dbb0733b2d84fa1c030cf9
Joseph Irwin <joseph.irwin.gt@gmail.com>
2014-04-01 18:30:43 +0900

    Play around with xcb

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 CXX=clang++
 CXXFLAGS+=-std=c++11
 CXXFLAGS+=-Wall -Wextra -pedantic
-CXXFLAGS+=`pkg-config --cflags sdl2`
-LDFLAGS+=`pkg-config --libs sdl2`
+CXXFLAGS+=`pkg-config --cflags xcb`
+LDFLAGS+=`pkg-config --libs xcb`
 
 run: main.cc sdl.cc
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o run main.cc sdl.cc
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o run main.cc
 
 clean:
 	rm -f run
--- main.cc
+++ main.cc
@@ -2,25 +2,109 @@
 #include <memory>
 #include <exception>
 #include <string>
+#include <vector>
 
-#include "sdl_wrap.h"
+#include <xcb/xcb.h>
+#include <unistd.h>
 
+namespace util {
+  template<typename Concrete, typename Handle>
+  struct handle_base {
+    typedef Concrete concrete_type;
+    typedef Handle dereferenced_type;
+    typedef Handle* handle_type;
+    
+    handle_base(handle_type h) : _handle(h) {}
+    handle_type handle() { return _handle.get(); }
+    
+  protected:
+    struct destroyer {
+      void operator()(handle_type h) {
+	concrete_type::destroy(h);
+      }
+    };
 
-int main(int /*argc*/, char **/*argv*/)
+    std::unique_ptr<dereferenced_type, destroyer> _handle;
+  };
+}
+
+namespace xcb {
+
+  struct window {
+    window(xcb_window_t id) : _id(id) {}
+    xcb_window_t id() const { return _id; }
+  private:
+    xcb_window_t _id;
+  };
+
+  struct connection : util::handle_base<connection, xcb_connection_t> {
+    typedef util::handle_base<connection, xcb_connection_t> base_type;
+    
+    connection(): base_type(nullptr), _screen(-1) {
+      _handle.reset(xcb_connect(nullptr, &_screen));
+    }
+    
+    int screen() const { return _screen; }
+
+    std::vector<xcb_screen_t> get_screens() {
+      std::vector<xcb_screen_t> vec;
+
+      auto it = xcb_setup_roots_iterator(xcb_get_setup(handle()));
+      for (; it.rem > 0; xcb_screen_next(&it)) {
+	vec.push_back(*(it.data));
+      }
+
+      return vec;
+    }
+    
+    window create_window(int x, int y, int w, int h) {
+      auto id = xcb_generate_id(handle());
+      auto screens = get_screens();
+      auto cookie = xcb_create_window(handle(),
+				      XCB_COPY_FROM_PARENT,
+				      id,
+				      screens[0].root,
+				      x, y, w, h,
+				      10,
+				      XCB_WINDOW_CLASS_INPUT_OUTPUT,
+				      screens[0].root_visual,
+				      0, nullptr);
+      return window(id);
+    }
+    void map(const window &w) {
+      xcb_map_window(handle(), w.id());
+    }
+    
+    void flush() { xcb_flush(handle()); }
+
+  private:
+    friend base_type;
+    static void destroy(xcb_connection_t *c) {
+      xcb_disconnect(c);
+    }
+    
+    int _screen;
+  };
+  
+}
+
+int main()
 {
-  sdl::initializer init;
+  xcb::connection c;
   
-  sdl::window win("Hello World!", 100, 100, 640, 480);
+  std::cout << "Current screen: " << c.screen() << std::endl;
 
-  sdl::surface bmp = sdl::load_bitmap("hello.bmp");
+  auto screens = c.get_screens();
+  for (int i = 0; i < screens.size(); ++i) {
+    auto &s = screens[i];
+    std::cout << "Screen " << i << ": " << s.width_in_pixels << "x" << s.height_in_pixels << std::endl;
+  }
   
-  sdl::texture tex = win.renderer().create_texture(bmp);
-
-  win.renderer().clear();
-  win.renderer().copy(tex);
-  win.renderer().present();
+  xcb::window w = c.create_window(0, 0, 150, 150);
+  c.map(w);
+  c.flush();
   
-  sdl::delay(2000);
+  pause();
 
   return 0;
 }
